20fe7ef730e512f0bbd8b731773795ca04d1913b
jordanzz <jjordanzlatanov@gmail.com>
2019-11-09 20:57:39 +0200

    Camera Screen

---
+++ scr/screens/camera-screen.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import { Button, View, Text } from 'react-native';
+import COLORS from '../../assets/styles/colors';
+import DESIGNS from '../../assets/styles/designs';
+import Camera from 'react-native-camera';
+
+export default class Camerascreen extends Component {
+  render() {
+    return (
+      <View>
+
+      </View>
+    )
+  }
+}
